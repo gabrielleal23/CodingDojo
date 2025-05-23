f31e5ad246a5f9f386351d1b338f56a5311dbb12
gabrielleal23 <110639928+gabrielleal23@users.noreply.github.com>
2025-05-22 20:57:32 -0500

    codificacion final

--- coding.py
+++ coding.py
@@ -6,12 +6,10 @@ def obtener_eleccion_computadora():
 
 def determinar_ganador(jugador,computadora):
     if jugador == computadora:
-        return "Empate"
-    elif (jugador == "piedra" and computadora == "tijera") or \
-    (jugador == "papel" and computadora == "piedra") or \
-    (jugador == "tijera" and computadora == "papel"):
+        return "Empate" 
+    elif (jugador == "Piedra" and computadora =="tijera") or (jugador == "papel" and computadora =="piedra") or (jugador == "tijera" and computadora =="papel"):
         return "Ganaste"
-    else : 
+    else:
         return "Perdiste"
     
 jugador = input("Elige piedra, papel o tijera: \n").lower()
